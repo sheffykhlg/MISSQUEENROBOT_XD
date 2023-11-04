96f6fffb1cef666c5cb3c5d3f3619dcc8191ec78
INNOCENTLDKAA <141138031+INNOCENTLDKAA@users.noreply.github.com>
2023-11-04 21:50:09 +0530

    Update aifunctions.py

--- MukeshRobot/modules/aifunctions.py
+++ MukeshRobot/modules/aifunctions.py
@@ -36,7 +36,7 @@ async def hmm(event):
         except CFError as e:
             print(e)
     elif "ann" in result:
-        pro = "My name is rocky"
+        pro = "My name is ɪɴɴᴏᴄᴇɴᴛ "
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
