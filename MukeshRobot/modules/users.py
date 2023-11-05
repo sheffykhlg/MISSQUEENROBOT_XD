b49e6f53e97af0edc843623b04c910a19a72ce4c
INNOCENTLDKAA <141138031+INNOCENTLDKAA@users.noreply.github.com>
2023-11-05 23:03:48 +0530

    Update users.py

--- MukeshRobot/modules/users.py
+++ MukeshRobot/modules/users.py
@@ -250,5 +250,5 @@ dispatcher.add_handler(BROADCAST_HANDLER)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
-__mod_name__ = "ᴜsᴇʀs"
+__mod_name__ = "⛧‌ᴜsᴇʀs⛧‌"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), BROADCAST_HANDLER, CHATLIST_HANDLER]
