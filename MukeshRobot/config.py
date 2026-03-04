25f53f8ffb5b97dd9985cd855574c1f1bcffec83
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2026-03-04 20:57:38 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,4 +1,3 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -8,63 +7,51 @@ def get_user_list(config, key):
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "28630777" # integer value, dont use ""
-    API_HASH = "41962bd1035fcbd5c0243cec216a6e23"
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "6545714937" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Itzz_me_innocentt"
-    SUPPORT_CHAT = "friendship_forever_group143"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001885729197
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001885729197
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority"
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    TOKEN = os.environ.get("TOKEN")
+    OWNER_ID = os.environ.get("OWNER_ID")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
+    JOIN_LOGGER = os.environ.get("JOIN_LOGGER")
+    EVENT_LOGS = os.environ.get("EVENT_LOGS")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
-    URL = None
-    SPAMWATCH_API = "ibGjchtz3djmcaIDiG3qoLX8KE0Npo1ChFiH7QGpjoQY1uB7BGOPJ~DOT07c0VSH"  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    URL = os.environ.get("URL")
 
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "6545714937")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "6545714937")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "6545714937")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "6545714937")
+    SPAMWATCH_API = os.environ.get("SPAMWATCH_API")
+    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT")
+
+    DRAGONS = get_user_list("elevated_users.json", "dragons")
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEMONS = get_user_list("elevated_users.json", "demons")
+    TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = "https://t.me/ROCKY_ISS_BACK" # EG, paypal
+
+    DONATION_LINK = os.environ.get("DONATION_LINK")
     CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    PORT = int(os.environ.get("PORT", 5000))
+
+    DEL_CMDS = True
     STRICT_GBAN = True
-    WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
-    )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = (
-        "7AT86JXWTFKSFSCQ"  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = "5LB4TAKPEKZ0"  # Get your API key from https://timezonedb.com/api
-    WALL_API = (
-        ""  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    )
-    BL_CHATS = []  # List of groups that you want blacklisted.
+    WORKERS = int(os.environ.get("WORKERS", 8))
+
+    BAN_STICKER = os.environ.get("BAN_STICKER")
+    ALLOW_EXCL = True
+
+    CASH_API_KEY = os.environ.get("CASH_API_KEY")
+    TIME_API_KEY = os.environ.get("TIME_API_KEY")
+    WALL_API = os.environ.get("WALL_API")
+
+    BL_CHATS = []
     SPAMMERS = None
 
 
